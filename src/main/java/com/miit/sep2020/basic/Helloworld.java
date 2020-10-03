b0517effdeb1eb50ca7844334f40449f2f97b09c
AnuragDhiman400 <anuragdhiman400@gmail.com>
2020-10-03 00:01:52 -0400

    first-commit

---
+++ src/main/java/com/miit/sep2020/basic/Helloworld.java
@@ -0,0 +1,10 @@
+package com.miit.sep2020.basic;
+
+public class Helloworld {
+	
+	public static void main (String arg[])
+	{
+		System.out.println("Hello world:");
+	}
+
+}
---
+++ src/test/java/com/miit/junit/sep2020/Javatest.java
@@ -0,0 +1,23 @@
+package com.miit.junit.sep2020;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+public class Javatest {
+	
+	@Test
+	public void testNumber1()
+	{
+		assertEquals(5, 5);
+		
+	}
+	@Test
+	public void testNumber2()
+	{
+		assertEquals(5, 6);
+		
+	}
+
+
+}
